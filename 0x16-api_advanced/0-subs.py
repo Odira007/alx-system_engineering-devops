a6e8a5de8903e0b414e2fdb1eab871d6b2e51df6
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-06-14 18:06:13 -0700

    Reddit API fetch

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""query the reddit api"""
-
+"""queries the Reddit API"""
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """returns total subs"""
     header = {"User-Agent": "Holberton"}
-    req = "https://www.reddit.com/r/" + subreddit + "/about.json"
-    res = requests.get(req, headers=header)
-    if (res.status_code == 200):
-        return res.json().get("data", None).get("subscribers", None)
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    r = requests.get(url, headers=header)
+    if r.status_code == 200:
+        return r.json().get("data", None).get("subscribers", None)
     else:
         return 0
+
 if __name__ == "__main__":
     pass
